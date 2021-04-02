c6a310fdb77f17611d56e7b7edf18a45944a95be
Zaman <zamanehsani@gmail.com>
2021-04-02 14:37:42 +0400

    initial commit

---
+++ website/coming/views.py
@@ -0,0 +1,26 @@
+from django.core.mail import send_mail
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView
+# Create your views here.
+
+def coming(request):
+    return render(request, 'coming_soon.html')
+
+def subscribe(request):
+    email = request.POST.get('email')
+    send_mail(
+        "Thank You",
+        "Dear visitor; thank you for subscribing. We will notify you when we go live.", 
+        'zamanehsani@outlook.com', 
+        [email] , 
+        fail_silently=False
+        )
+    messages.success(request, 'Thank you for subscribing.')
+    return redirect('coming_soon')
+
+class Main(ListView):
+    template_name = 'main.html'
+    model = User
---
+++ website/website/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from coming import views as coming_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('coming-soon', coming_views.coming, name="coming_soon"),
+    path('subscribe', coming_views.subscribe, name="subscribe"),
+    path('', coming_views.Main.as_view(), name='main'),
+]
